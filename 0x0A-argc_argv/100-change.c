7a39d7bc665691c6d77f079d5f36213a5e36e159
SelloaneMachitje <machitjeselloane@gmail.com>
2023-10-02 12:58:59 -0700

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int num, j, reult;
+	int num, j, result;
 	int coins [] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 
 	if (num < 0)
 	{
-		prinf("0\n");
+		printf("0\n");
 		return (0);
 	}
 
