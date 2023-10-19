819badf1bd936d51b5e2e51919d25179b85e1abf
SelloaneMachitje <machitjeselloane@gmail.com>
2023-10-19 06:35:13 -0700

    update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,16 +4,15 @@
 /**
  * free_list - frees a linked list
  * @head: list_t list tobe freed
+ *
+ * Return: void
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
-
 	while (head)
 	{
-		temp = head->next;
 		free(head->str);
 		free(head);
-		head = temp;
+		head = head->next;
 	}
 }
