3bcadecc287367dbeafccbc3420ed979442c5176
SelloaneMachitje <machitjeselloane@gmail.com>
2023-10-19 20:13:46 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,23 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *list;
+	unsigned int index = 0;
 
-	while (str[len])
-		len++;
-	new = malloc(sizeof(list_t));
-	if (!new)
+	list = malloc(sizeof(list_t));
 
+	if (list == NULL)
+	{
 		return (NULL);
+	}
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
-
+	while (str[index] != '\0')
+	{
+		index++;
+	}
+	list->len = index;
+	list->str = strdup(str);
+	list->next = *head;
+	*head = list;
 	return (*head);
 }
